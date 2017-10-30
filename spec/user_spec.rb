dbf72d9e7214392eb67595527028fcf56490bce4
Eugis <eugenijus.sersniovas@gmail.com>
2017-10-31 01:41:31 +0200

    Added 5 tests to test log in functionality

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -25,16 +25,49 @@ describe User do
   end
 
   context 'user info validation' do
-    it 'warns if there is spaces in username' do
+    it 'raises error if there is spaces in username' do
       expect { user.create('bo b', 'west') }.to raise_error(
         ArgumentError, 'No spaces allowed in username!'
       )
     end
 
-    it 'warns if there is spaces in password' do
+    it 'raises error if there is spaces in password' do
       expect { user.create('bob', 'w e st') }.to raise_error(
         ArgumentError, 'No spaces allowed in password!'
       )
+    end
+  end
+
+  context 'bad login info' do
+    it 'warns if there is no such user' do
+      user.create('bob', 'one')
+      expect { user.login('john', 'three') }.to output(
+        "There is no such user registered\n"
+      ).to_stdout
+    end
+
+    it 'raiser error if entered password is wrong' do
+      user.create('bob', 'one')
+      expect { user.login('bob', 'onee') }.to raise_error(
+        ArgumentError, 'Wrong password!'
+      )
+    end
+
+    it 'checks user list for entered user info' do
+      user.create('bob', 'west')
+      expect(user.find_user('john')).to eq false
+    end
+  end
+
+  context 'log in status' do
+    it 'set to false on initialize' do
+      expect(user.log_in). to eq false
+    end
+
+    it 'set to true after login' do
+      user.create('bob', 'one')
+      user.login('bob', 'one')
+      expect(user.log_in).to eq true
     end
   end
 end
